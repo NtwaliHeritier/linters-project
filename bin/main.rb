e6b2a63b3d5605b25d84f739704b12e6927e4eeb
NtwaliHeritier <heritierntwali@gmail.com>
2020-10-01 20:57:31 +0200

    add the file_test.rb file in the lib directory

--- bin/main.rb
+++ bin/main.rb
@@ -1,11 +1,19 @@
+require_relative '../lib/file_test.rb'
+
 file_path = ""
 puts "Welcome to the linter"
  loop do
     puts  "Please enter the path to your file"
     file_path = gets.chomp
-    break if File.exists?(file_path) 
-    puts "File doesn't exist"
+    if File.exists?(file_path)&&file_path.include?(".js")
+        break
+    elsif File.exists?(file_path)
+        puts "wrong file"
+    else
+        puts "File doesn't exist"
+    end
  end
 
-puts File.read(file_path)
+ test=Test.new(file_path)
+ puts test.file
 
---
+++ lib/file_test.rb
@@ -0,0 +1,6 @@
+class Test
+    attr_accessor :file
+    def initialize(file)
+      @file = file
+    end
+end
